50fd67c43c67a0fc39ca15e25d8b605947d2a2b0
Gashishiin <gashishrf@list.ru>
2018-09-04 11:48:58 +0300

    Added dependencies

--- src/main/java/com/randominfo/bot/services/Main.java
+++ src/main/java/com/randominfo/bot/services/Main.java
@@ -2,11 +2,13 @@ package com.randominfo.bot.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(Main.class);
     }
 }
